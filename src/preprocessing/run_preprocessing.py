c3145038fdc4c0d1663f5e924bf8b2b822303994
AndrewVFranco <129307231+AndrewVFranco@users.noreply.github.com>
2026-03-21 01:49:48 -0700

    Add multiprocessing with tqdm progress bar to processing runner

--- src/preprocessing/run_preprocessing.py
+++ src/preprocessing/run_preprocessing.py
@@ -1,29 +1,31 @@
 from src.preprocessing.preprocess import preprocess_case
 from pathlib import Path
 from tqdm import tqdm
+from functools import partial
+import multiprocessing
 import sys
 
 def main():
+    multiprocessing.set_start_method('fork', force=True)
     PROJECT_ROOT = Path(__file__).resolve().parents[2]
     DATA_DIR = PROJECT_ROOT / "data" / "raw" / "ASNR-MICCAI-BraTS2023-GLI-Challenge-TrainingData"
     OUTPUT_DIR = PROJECT_ROOT / "data" / "processed"
 
+    preprocess_with_output = partial(preprocess_case, output_dir=OUTPUT_DIR)
+
     cases = sorted(DATA_DIR.iterdir())
-    failed_cases = []
 
-    cases = cases[:1]
-    for case in tqdm(cases):
+    N = multiprocessing.cpu_count() - 2
+
+    with multiprocessing.Pool(processes=N) as pool:
         try:
-            preprocess_case(case, OUTPUT_DIR)
+            results = list(tqdm(
+                pool.imap(preprocess_with_output, cases),
+                total=len(cases)
+            ))
+            print("\nAll cases processed successfully.")
         except Exception as e:
-            print(f"case:{case} failed, error {e}")
-            failed_cases.append(case)
-            continue
-
-    if failed_cases:
-        print(f"\n{len(failed_cases)} cases failed: {failed_cases}")
-    else:
-        print("\nAll cases processed successfully.")
+            print(f"\nPreprocessing failed: {e}")
 
     return 0
 
